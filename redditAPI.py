9330ac184ceaacba4aa2beeb4be415a377c2619d
Priya Shah <shahpriya890@gmail.com>
2021-04-23 15:56:23 -0400

    Get comments

--- redditAPI.py
+++ redditAPI.py
@@ -11,9 +11,14 @@ reddit = praw.Reddit(client_id = '59cxBcvPTe0WpA',
 subreddit = reddit.subreddit('UPenn')
 
 # get the top 5 hot posts
-hot_posts = subreddit.hot(limit = 5)
+hot_posts = subreddit.hot(limit = 1)
 
 # print the titles 
 for submission in hot_posts:
     print(submission.title)
+    print(50*'-')
+    comments = submission.comments
+    for comment in comments:
+        print(50*'*')
+        print(comment.body)
 
