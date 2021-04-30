03b4fd1c674bd4249f36c375eec6bd0a4838a7e9
Saurabh Shah <surb@seas.upenn.edu>
2021-04-30 14:52:50 -0400

    Update redditAPI.py

--- redditAPI.py
+++ redditAPI.py
@@ -8,6 +8,9 @@ reddit = praw.Reddit(client_id = '59cxBcvPTe0WpA',
                      user_agent = "csScraper v1.0 (by /u/netsReddit)")
 
 # subreddit of interest
+subreddits = ['CMU', 'UIUC', 'MIT', 'stanford', 'UCSD', 'berkeley', 'cornell', 'umich', 'udub', 'umd', 'gatech', 
+              'neu', 'columbia', 'uwmadison', 'upenn', 'utaustin' , 'purdue', 'umass', 'nyu', 'ucla']
+
 subreddit = reddit.subreddit('UPenn')
 
 # get the top 5 hot posts
