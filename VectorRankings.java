06b685257c86f3c8784f5ac4f411c9b4d8103e54
Priya Shah <shahpriya890@gmail.com>
2021-05-01 20:19:03 -0400

    Update VR

---
+++ VectorRankings.java
@@ -0,0 +1,14 @@
+package ir;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+public class VectorRankings {
+
+    public static void main(String[] args) throws IOException {
+
+    }
+
+}
